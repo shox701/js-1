64dc10ec9ae153b7dadba09ae3c318acc0c8ec3c
shox701 <ebrbrn71@gmail.com>
2023-12-14 23:12:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+alert("Assalomu alaykum")
+var ism = prompt("ISMINGIZNI KIRITING")
+console.log(ism);
+var tel = prompt("TELEFON RAQAMINGIZNI KIRITING")
+console.log(tel);
+var yosh = prompt("YOSHINGIZNI KIRITING")
+console.log(yosh);
